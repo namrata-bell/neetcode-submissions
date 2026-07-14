5dfee09f6e96ef838c5210d11f4788fe39788600
Namrata <namratabellenavar@gmail.com>
2026-07-14 22:17:50 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+//SEE AGAIN
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* groupPrev = &dummy;
+
+        while (true) {
+
+            // Find kth node
+            ListNode* kth = groupPrev;
+
+            for (int i = 0; i < k && kth; i++)
+                kth = kth->next;
+
+            if (!kth)
+                break;
+
+            ListNode* groupNext = kth->next;
+
+            // Reverse current group
+            ListNode* prev = groupNext;
+            ListNode* curr = groupPrev->next;
+
+            while (curr != groupNext) {
+                ListNode* temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            // Connect reversed group
+            ListNode* temp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = temp;
+        }
+
+        return dummy.next;
+    }
+};
