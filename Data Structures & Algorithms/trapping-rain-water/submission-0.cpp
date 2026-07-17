ffe8259105cececf27cab21492a6559ea0d41807
Namrata <namratabellenavar@gmail.com>
2026-07-17 09:09:59 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,63 @@
+// class Solution {//tc=sc=O(n)
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+
+//         vector<int> leftMax(n), rightMax(n);
+
+//         leftMax[0] = height[0];
+//         for (int i = 1; i < n; i++)
+//             leftMax[i] = max(leftMax[i - 1], height[i]);
+
+//         rightMax[n - 1] = height[n - 1];
+//         for (int i = n - 2; i >= 0; i--)
+//             rightMax[i] = max(rightMax[i + 1], height[i]);
+
+//         int ans = 0;
+
+//         for (int i = 0; i < n; i++)
+//             ans += min(leftMax[i], rightMax[i]) - height[i];
+
+//         return ans;
+//     }
+// };
+
+//optimal
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int left = 0;
+        int right = height.size() - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
+
+                left++;
+            }
+            else {
+
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+};//tc=O(n)...sc=O(1)
