8fb971f467668afd082458da5e9225a3fa691529
Namrata <namratabellenavar@gmail.com>
2026-08-02 22:58:45 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> subset;
+
+    void backtrack(vector<int>& nums, int index) {
+
+        if (index == nums.size()) {
+            ans.push_back(subset);
+            return;
+        }
+
+        // Include current element
+        subset.push_back(nums[index]);
+        backtrack(nums, index + 1);
+
+        // Backtrack
+        subset.pop_back();
+
+        // Exclude current element
+        backtrack(nums, index + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return ans;
+    }
+};
