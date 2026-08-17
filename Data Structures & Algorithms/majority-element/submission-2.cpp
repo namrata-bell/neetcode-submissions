1e325801ec4c39447b37183a24fb1c548c656e15
Namrata <namratabellenavar@gmail.com>
2026-08-17 15:12:23 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans=0;
+        int cnt=0;
+        for(int num:nums){
+            if(cnt==0)ans=num;
+           if(ans==num)cnt++;
+           else cnt--;
+        }
+      return ans;
+    }
+};
