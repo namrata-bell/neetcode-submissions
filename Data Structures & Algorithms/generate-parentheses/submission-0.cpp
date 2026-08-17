8d1e36e89d827ce4e1db6d63164b7750fd6e95c1
Namrata <namratabellenavar@gmail.com>
2026-08-17 09:34:55 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+vector<string>res;
+void backtrack(string s,int open,int close,int n){
+    if(s.size()==2*n){
+        res.push_back(s);
+        return;
+    }
+    if(open<n){
+        backtrack(s+"(",open+1,close,n);
+    }
+    if(close<open){
+        backtrack(s+")",open,close+1,n);
+    }
+}
+    vector<string> generateParenthesis(int n) {
+        backtrack("",0,0,n);
+        return res;
+    }
+};
